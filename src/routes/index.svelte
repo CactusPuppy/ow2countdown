90df7789b262854e39555cae97c7dbdcc1b25863
CactusPuppy <puppycactus@gmail.com>
2022-04-14 20:02:27 -0700

    chore: Initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+
+</script>
+
+<div class="flex flex-col justify-center items-center h-[100vh] w-[100vw]">
+  <div class="flex-grow flex flex-col justify-center items-center py-8">
+    <p>Hello!</p>
+  </div>
+  <div class="flex justify-between my-4 mx-2 px-4 h-8 w-full">
+    <p class="text-xs text-gray-500 dark:text-slate-400 italic text-left">Created by <a class="text-blue-600 dark:text-blue-300 underline" href="https://twitter.com/Cactus_Puppy" rel="noreferrer noopener" target="_blank">@Cactus_Puppy</a><br /><a href="https://github.com/CactusPuppy/isow2outyet" rel="noreferrer noopener" target="_blank" class="text-blue-600 dark:text-blue-300 underline">GitHub</a></p>
+    <p class="text-xs text-gray-500 dark:text-slate-400 italic text-right">
+      This site and its creator are not affiliated with Overwatch or Blizzard Entertainment.
+      <br />Overwatch 2 and the Overwatch 2 logo are ©2022 Blizzard Entertainment, Inc.
+    </p>
+  </div>
+</div>

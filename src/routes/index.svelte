2a9745ce2903f373833abcca49f8074d259954d2
CactusPuppy <puppycactus@gmail.com>
2022-04-26 10:25:30 -0700

    fix: Fix GitHub link

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -47,7 +47,7 @@
     <Timer start={now} end={new Date("2022-04-26T11:00:00.000-07:00")} />
   </div>
   <div class="flex justify-between my-4 mx-2 px-4 h-8 w-full">
-    <p class="text-xs text-gray-500 dark:text-slate-400 italic text-left">Created by <a class="text-blue-600 dark:text-blue-300 underline" href="https://twitter.com/Cactus_Puppy" rel="noreferrer noopener" target="_blank">@Cactus_Puppy</a><br /><a href="https://github.com/CactusPuppy/isow2outyet" rel="noreferrer noopener" target="_blank" class="text-blue-600 dark:text-blue-300 underline">GitHub</a></p>
+    <p class="text-xs text-gray-500 dark:text-slate-400 italic text-left">Created by <a class="text-blue-600 dark:text-blue-300 underline" href="https://twitter.com/Cactus_Puppy" rel="noreferrer noopener" target="_blank">@Cactus_Puppy</a><br /><a href="https://github.com/CactusPuppy/ow2countdown" rel="noreferrer noopener" target="_blank" class="text-blue-600 dark:text-blue-300 underline">GitHub</a></p>
     <p class="text-xs text-gray-500 dark:text-slate-400 italic text-right">
       This site and its creator are not affiliated with Overwatch or Blizzard Entertainment.
       <br />Overwatch 2 and the Overwatch 2 logo are ©2022 Blizzard Entertainment, Inc.

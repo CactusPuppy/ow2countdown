709a465373991e4965de18ae5382f4dec3f2bb7e
CactusPuppy <puppycactus@gmail.com>
2022-04-27 09:19:42 -0700

    feat: Add automatic rollover

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -15,6 +15,7 @@
   import { getDates, dates } from "../stores/dates";
   import Timer from "./_timer.svelte";
   import { onMount } from "svelte";
+import { compareAsc, isPast, parseISO } from "date-fns";
 
   export let _dates : CountdownDate[];
   if (!$dates.length) $dates = _dates;
@@ -33,21 +34,38 @@
 
   function updateTime() {
     now = new Date();
+    if (compareAsc(now, dateMarker) > 0) {
+      // TODO: Move to next event
+      activeDate = {
+        id: 1,
+        date: "2022-04-27T18:00:00.000-07:00",
+        title: "Twitch Drops End for OW2 Beta Access",
+        description: ""
+      }
+    }
     setTimeout(updateTime, 100);
   }
 
   onMount(() => {
     updateTime();
   });
+
+  let activeDate : CountdownDate = {
+    id: 0,
+    date: "2022-04-27T10:00:00.000-07:00",
+    title: "Twitch Drops for Overwatch 2 Beta 1 Access",
+    description: ""
+  };
+  $: dateMarker = parseISO(activeDate.date);
 </script>
 
 <div class="flex flex-col justify-center items-center h-[100vh] w-[100vw] dark:bg-slate-800">
   <div class="flex-grow flex flex-col justify-center items-center gap-6 py-8 w-[100vw] dark:text-gray-50">
     <p class="text-center">
       <b class="text-3xl">Current Countdown:</b>
-      <br>Twitch Drops for Overwatch 2 Beta 1 Access
+      <br>{activeDate.title}
     </p>
-    <Timer start={now} end={new Date("2022-04-27T10:00:00.000-07:00")} /> <!-- TODO: Make this actually pull from the dates -->
+    <Timer start={now} end={dateMarker} /> <!-- TODO: Make this actually pull from the dates -->
   </div>
   <div class="flex justify-between my-4 mx-2 px-4 h-8 w-full">
     <p class="text-xs text-gray-500 dark:text-slate-400 italic text-left">Created by <a class="text-blue-600 dark:text-blue-300 underline" href="https://twitter.com/Cactus_Puppy" rel="noreferrer noopener" target="_blank">@Cactus_Puppy</a><br /><a href="https://github.com/CactusPuppy/ow2countdown" rel="noreferrer noopener" target="_blank" class="text-blue-600 dark:text-blue-300 underline">GitHub</a></p>

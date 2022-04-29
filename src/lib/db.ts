afeba93977cdaa82b7ce2435996ad01bdb90e190
CactusPuppy <puppycactus@gmail.com>
2022-04-28 17:55:51 -0700

    chore: Use env vars instead of hardcoded credentials

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,7 +1,6 @@
 import {createClient} from "@supabase/supabase-js";
 
-// TODO: Replace with environment variables
 export default createClient(
-  "https://swehgizakujrcplljupc.supabase.com",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN3ZWhnaXpha3VqcmNwbGxqdXBjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDk5OTE2NzYsImV4cCI6MTk2NTU2NzY3Nn0.nNPzoQ7vrD4sGA_-UY5h9Tb-2dbD85eFXeBL-FfRw0M",
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY,
 );

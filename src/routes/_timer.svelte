9848fa26fe044d1eb8a7cfc51ace056c967b85d3
CactusPuppy <puppycactus@gmail.com>
2022-05-16 23:04:10 -0700

    fix: prevent excessive padding around minutes

--- src/routes/_timer.svelte
+++ src/routes/_timer.svelte
@@ -14,7 +14,7 @@
   $: minutesToGo = Math.floor(totalSeconds / 60) % 60;
   $: secondsToGo = Math.floor(totalSeconds % 60);
   $: timerValues = [].concat([daysToGo, hoursToGo, minutesToGo, secondsToGo]);
-  let timerUnits = ["days", "hours", "minutes", "seconds"];
+  let timerUnits = ["days", "hours", "mins", "seconds"];
 
   $: hideValue = start == null || end == null;
 </script>

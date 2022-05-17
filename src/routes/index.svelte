fab5f805c22523524ea0f0651c37b091657c0e68
CactusPuppy <puppycactus@gmail.com>
2022-05-16 17:32:30 -0700

    feat: Show local time

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,7 +3,7 @@
   import { getDates, dates } from "../stores/dates";
   import Timer from "./_timer.svelte";
   import { onMount } from "svelte";
-  import { addSeconds, compareAsc, formatISO, getMilliseconds, parseISO } from "date-fns";
+  import { addSeconds, compareAsc, format, formatISO, getMilliseconds, parseISO } from "date-fns";
 
   let now : Date;
   let activeDate : CountdownDate;
@@ -84,12 +84,19 @@
 </script>
 
 <div class="flex flex-col justify-center items-center h-[100vh] w-[100vw]">
-  <div class="flex-grow flex flex-col justify-center items-center gap-6 py-8 w-[100vw] dark:text-zinc-50">
-    <p class="text-center text-xl md:text-2xl lg:text-3xl">
-      <b class="text-3xl md:text-4xl lg:text-5xl">Current Countdown:</b>
-      <br><span class="text-ow2-orange dark:text-ow2-light-orange">{activeDate ? activeDate.title : "Loading..."}</span>
+  <div class="flex-grow flex flex-col justify-center items-center py-8 w-[100vw] dark:text-zinc-50">
+    <p class="text-center text-3xl md:text-4xl lg:text-5xl">
+      <b>Current Countdown:</b>
     </p>
-    <Timer start={now} end={dateMarker} />
+    <p class="text-center text-xl md:text-2xl lg:text-3xl text-ow2-orange dark:text-ow2-light-orange">
+      {activeDate ? activeDate.title : "Loading..."}
+    </p>
+    <p class="text-center text-md md:text-xl lg:text-2xl">
+      {activeDate ? format(parseISO(activeDate?.date), "PPPPp") : ""}
+    </p>
+    <div class="mt-6">
+      <Timer start={now} end={dateMarker} />
+    </div>
   </div>
   <div class="flex justify-between my-4 mx-2 px-4 h-8 w-full">
     <p class="text-xs text-zinc-500 dark:text-zinc-400 italic text-left">Created by <a class="text-ow2-orange dark:text-ow2-light-orange underline" href="https://twitter.com/Cactus_Puppy" rel="noreferrer noopener" target="_blank">@Cactus_Puppy</a><br /><a href="https://github.com/CactusPuppy/ow2countdown" rel="noreferrer noopener" target="_blank" class="text-ow2-orange dark:text-ow2-light-orange underline">GitHub</a></p>

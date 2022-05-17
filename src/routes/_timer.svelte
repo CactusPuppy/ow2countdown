20d1fd2ae2bfb2cec6870225a77ef616faf049a4
CactusPuppy <puppycactus@gmail.com>
2022-05-16 17:48:15 -0700

    feat: Fade in elements on load

--- src/routes/_timer.svelte
+++ src/routes/_timer.svelte
@@ -2,6 +2,8 @@
   import { differenceInSeconds } from "date-fns";
   import TimerSegment from "./_timer_segment.svelte";
 
+  import { fade } from "svelte/transition";
+
   const SECONDS_IN_A_DAY = 86400;
 
   export let start : Date;
@@ -11,18 +13,21 @@
   $: hoursToGo = Math.floor(totalSeconds / 3600);
   $: minutesToGo = Math.floor(totalSeconds / 60) % 60;
   $: secondsToGo = Math.floor(totalSeconds % 60);
+
+  $: hideValue = start == null || end == null;
 </script>
 
 <div class="flex w-72 md:w-96 lg:w-[30rem] justify-between">
-  { #if start == null || end == null }
-    <TimerSegment value={null} unit="days" />
-    <TimerSegment value={null} unit="hours" />
-    <TimerSegment value={null} unit="mins" />
-    <TimerSegment value={null} unit="secs" />
-  { :else }
-    <TimerSegment value={daysToGo} unit="days" />
-    <TimerSegment value={hoursToGo} unit="hours" />
-    <TimerSegment value={minutesToGo} unit="mins" />
-    <TimerSegment value={secondsToGo} unit="secs" />
-  { /if }
+  <div in:fade="{{duration: 500, delay: 0}}">
+    <TimerSegment value={hideValue ? null : daysToGo} unit="days"    />
+  </div>
+  <div in:fade="{{duration: 500, delay: 300}}">
+    <TimerSegment value={hideValue ? null : hoursToGo} unit="hours"  />
+  </div>
+  <div in:fade="{{duration: 500, delay: 600}}">
+    <TimerSegment value={hideValue ? null : minutesToGo} unit="mins" />
+  </div>
+  <div in:fade="{{duration: 500, delay: 900}}">
+    <TimerSegment value={hideValue ? null : secondsToGo} unit="secs" />
+  </div>
 </div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,7 +3,10 @@
   import { getDates, dates } from "../stores/dates";
   import Timer from "./_timer.svelte";
   import { onMount } from "svelte";
-  import { addSeconds, compareAsc, format, formatISO, getMilliseconds, parseISO } from "date-fns";
+  import { addSeconds, compareAsc, format, formatISO, parseISO } from "date-fns";
+  import { browser } from "$app/env";
+
+  import { fade } from "svelte/transition";
 
   let now : Date;
   let activeDate : CountdownDate;
@@ -77,26 +80,34 @@
     setTimeout(updateTime, 100);
   }
 
+  let visible = browser ? false : true;
   onMount(() => {
     updateDates();
     updateTime();
+    visible = true;
   });
 </script>
 
-<div class="flex flex-col justify-center items-center h-[100vh] w-[100vw]">
+<div class="flex flex-col justify-center items-center h-[100vh] max-h-[100vh] w-[100vw]">
   <div class="flex-grow flex flex-col justify-center items-center py-8 w-[100vw] dark:text-zinc-50">
-    <p class="text-center text-3xl md:text-4xl lg:text-5xl">
-      <b>Current Countdown:</b>
-    </p>
-    <p class="text-center text-xl md:text-2xl lg:text-3xl text-ow2-orange dark:text-ow2-light-orange">
-      {activeDate ? activeDate.title : "Loading..."}
-    </p>
-    <p class="text-center text-md md:text-xl lg:text-2xl">
-      {activeDate ? format(parseISO(activeDate?.date), "PPPPp") : ""}
-    </p>
-    <div class="mt-6">
-      <Timer start={now} end={dateMarker} />
-    </div>
+    {#if visible}
+      <div>
+        <p class="text-center text-3xl md:text-4xl lg:text-5xl">
+          <b>Current Countdown:</b>
+        </p>
+        {#key activeDate?.id}
+          <p class="mt-1 text-center text-xl md:text-2xl lg:text-3xl text-ow2-orange dark:text-ow2-light-orange" in:fade="{{duration: 500}}">
+            {activeDate ? activeDate.title : "Loading..."}
+          </p>
+          <p class="text-center text-md md:text-xl lg:text-2xl" in:fade="{{duration: 500, delay: 300}}">
+            {activeDate && activeDate.id != -1 ? format(parseISO(activeDate?.date), "PPPPp") : " "}
+          </p>
+        {/key}
+      </div>
+      <div class="mt-8" in:fade="{{duration: 800}}">
+        <Timer start={now} end={dateMarker} />
+      </div>
+    {/if}
   </div>
   <div class="flex justify-between my-4 mx-2 px-4 h-8 w-full">
     <p class="text-xs text-zinc-500 dark:text-zinc-400 italic text-left">Created by <a class="text-ow2-orange dark:text-ow2-light-orange underline" href="https://twitter.com/Cactus_Puppy" rel="noreferrer noopener" target="_blank">@Cactus_Puppy</a><br /><a href="https://github.com/CactusPuppy/ow2countdown" rel="noreferrer noopener" target="_blank" class="text-ow2-orange dark:text-ow2-light-orange underline">GitHub</a></p>

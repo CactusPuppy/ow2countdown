7d7aae54438837aa2d18e6c1026a130b1ef90085
CactusPuppy <puppycactus@gmail.com>
2022-05-19 18:37:27 -0700

    chore: Add easy way to mock dates

--- src/stores/dates.ts
+++ src/stores/dates.ts
@@ -1,6 +1,7 @@
 import client from "$lib/db";
 
 import type { CountdownDate } from "$lib/types";
+import { addSeconds, addMinutes, addHours, addDays, formatISO } from "date-fns";
 import { writable } from "svelte/store";
 
 const tableName = "Upcoming Dates";
@@ -9,6 +10,14 @@ const select = `id, title, description, date`;
 export const dates = writable([] as CountdownDate[]);
 
 export async function getDates(): Promise<CountdownDate[]> {
+  // return [
+  //   {
+  //     id: 1,
+  //     title: "The End of the World",
+  //     description: "The end of the world is coming",
+  //     date: formatISO(addSeconds(addDays(new Date(), 1), 7)),
+  //   }
+  // ]
   const { data, error } = await client.from(tableName).select(select).order("date", {ascending: true});
 
   if (error) throw new Error(error.message);

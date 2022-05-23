7bc20437cba28c653c2e30ba1b24c61744b9868d
CactusPuppy <puppycactus@gmail.com>
2022-05-23 12:01:07 -0700

    refactor: Allow Dates store to paginate

--- src/stores/dates.ts
+++ src/stores/dates.ts
@@ -8,8 +8,9 @@ const tableName = "Upcoming Dates";
 const select = `id, title, description, date`;
 
 export const dates = writable([] as CountdownDate[]);
+export const DATES_PAGE_SIZE = 10;
 
-export async function getDates(): Promise<CountdownDate[]> {
+export async function getDates(page = 0): Promise<CountdownDate[]> {
   // return [
   //   {
   //     id: 1,
@@ -18,7 +19,10 @@ export async function getDates(): Promise<CountdownDate[]> {
   //     date: formatISO(addSeconds(addDays(new Date(), 1), 7)),
   //   }
   // ]
-  const { data, error } = await client.from(tableName).select(select).order("date", {ascending: true});
+  const { data, error } = await client.from(tableName)
+  .select(select)
+  .order("date", {ascending: true})
+  .range(page * DATES_PAGE_SIZE, (page + 1) * DATES_PAGE_SIZE);
 
   if (error) throw new Error(error.message);
 

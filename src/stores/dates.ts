4f98f0a01c0bbd9d59193017eabd5be7db656091
CactusPuppy <puppycactus@gmail.com>
2022-06-13 11:35:52 -0700

    chore: Add absolute timestamp example to date stub

--- src/stores/dates.ts
+++ src/stores/dates.ts
@@ -16,7 +16,13 @@ export async function getDates(page = 0): Promise<CountdownDate[]> {
   //     id: 1,
   //     title: "The End of the World",
   //     description: "The end of the world is coming",
-  //     date: formatISO(addSeconds(addDays(new Date(), 1), 7)),
+  //     date: "2022-06-08T17:10:00-07:00",
+  //   },
+  //   {
+  //     id: 2,
+  //     title: "The End of the World 2",
+  //     description: "The end of the world is coming again",
+  //     date: formatISO(addSeconds(addDays(new Date(), 1), 17)),
   //   }
   // ]
   const { data, error } = await client.from(tableName)

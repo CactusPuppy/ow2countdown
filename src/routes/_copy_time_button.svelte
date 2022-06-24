9944eed5943c771ecf359352ede5f027014ba80b
Martín (Netux) Rodríguez <send.email@netux.site>
2022-06-24 18:53:29 -0300

    feat: Use Element.animate to fade in/out the "copied" label

--- src/routes/_copy_time_button.svelte
+++ src/routes/_copy_time_button.svelte
@@ -1,24 +1,32 @@
 <script lang="ts">
   import DropdownItem from "./_dropdown_item.svelte";
-  import { fade } from "svelte/transition";
 
   export let handleCopy : (() => Promise<void>);
 
   async function handleClick() {
     await handleCopy();
-    showConfirmation = true;
+    fadeInOutConfirmation();
   }
 
-  let showConfirmation = false;
+  let confirmationLabel: HTMLSpanElement;
+  function fadeInOutConfirmation() {
+    confirmationLabel.animate(
+      [
+        { opacity: 0, offset: 0 },
+        { opacity: 1, offset: 0.05 },
+        { opacity: 1, offset: 0.95 },
+        { opacity: 0, offset: 1.0 }
+      ],
+      1500
+    );
+  }
 </script>
 
 <DropdownItem slot="items" handleClick={handleClick}>
   <slot />
-  {#if showConfirmation}
-    <span
-      transition:fade
-      on:introend={() => { showConfirmation = false; }}
-      class="text-xs text-gray-900 text-right pl-2"
-    >(copied)</span>
-  {/if}
+  <span
+    bind:this={confirmationLabel}
+    style="opacity: 0"
+    class="text-xs text-gray-900 text-right pl-2"
+  >(copied)</span>
 </DropdownItem>

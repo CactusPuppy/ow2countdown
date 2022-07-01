ac9e6ffce3cfe8de390e237544172e77b271ad0e
CactusPuppy <puppycactus@gmail.com>
2022-07-01 13:41:58 -0700

    fix: Enable FontAwesome to build correctly

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,12 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		vite: {
+			ssr: {
+				noExternal: ['@fortawesome/free-solid-svg-icons', '@fortawesome/free-brands-svg-icons'],
+			}
+		}
 	}
 };
 

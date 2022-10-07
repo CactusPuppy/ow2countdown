195ee21cb42021b3d69b4dcc38b00b3cdf7f4c52
CactusPuppy <puppycactus@gmail.com>
2022-10-06 17:59:35 -0700

    feat: Add date grouping to avoid showing too many things at once

--- src/stores/dates.ts
+++ src/stores/dates.ts
@@ -1,28 +1,41 @@
 import client from "$lib/db";
 
 import type { CountdownDate } from "$lib/types";
-import { addSeconds, addMinutes, addHours, addDays, formatISO } from "date-fns";
+// import { addSeconds, addMinutes, addHours, addDays, formatISO, parseISO } from "date-fns";
 import { writable } from "svelte/store";
 
 const tableName = "Upcoming Dates";
-const select = `id, title, description, date`;
+const select = `id, title, description, date, group`;
 
 export const dates = writable([] as CountdownDate[]);
 export const DATES_PAGE_SIZE = 10;
 
 export async function getDates(page = 0): Promise<CountdownDate[]> {
+  // const timeInTheFuture = "2022-10-06T17:57:00-07:00";
   // return [
   //   {
   //     id: 1,
   //     title: "The End of the World",
   //     description: "The end of the world is coming",
-  //     date: "2022-06-08T17:10:00-07:00",
+  //     date: timeInTheFuture,
+  //     priority: 0,
+  //     group: "ohno",
   //   },
   //   {
   //     id: 2,
   //     title: "The End of the World 2",
   //     description: "The end of the world is coming again",
-  //     date: formatISO(addSeconds(addDays(new Date(), 1), 17)),
+  //     date: formatISO(addSeconds(addDays(parseISO(timeInTheFuture), 1), 17)),
+  //     priority: 0,
+  //     group: "ohno",
+  //   },
+  //   {
+  //     id: 3,
+  //     title: "A new element",
+  //     description: "asdf",
+  //     date: formatISO(addSeconds(addDays(parseISO(timeInTheFuture), 1), 17)),
+  //     priority: 0,
+  //     group: null
   //   }
   // ]
   const { data, error } = await client.from(tableName)

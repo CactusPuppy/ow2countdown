a0179efda064582146c49fb77632c9476178ae26
CactusPuppy <puppycactus@gmail.com>
2022-10-14 13:05:09 -0700

    feat: Copy update on error

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -65,7 +65,7 @@
 
 {#if $dates.errored}
   <div class="fixed top-0 z-30 w-full text-center p-4 bg-[#f15047df] dark:bg-[#7F1D1DDF] dark:text-zinc-50">
-    A problem arose while fetching the dates. Don't refresh, we'll try to contact the server again for you {nextAttemptMarker !== undefined && compareAsc(now, nextAttemptMarker) < 0 ? `in ${timeToNextAttempt}` : "soon"}.</div>
+    A problem arose while contacting the server for updated information. Don't refresh, we'll try to contact the server again for you {nextAttemptMarker !== undefined && compareAsc(now, nextAttemptMarker) < 0 ? `in ${timeToNextAttempt}` : "soon"}.</div>
 {/if}
 <div class="flex-grow flex flex-col gap-8 md:justify-center items-center px-4 py-8 w-full dark:text-zinc-50">
   {#if displayDates?.length != undefined}

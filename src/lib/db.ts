055a2a55152d9a5a43dc48e187ad96f9ef05f665
CactusPuppy <puppycactus@gmail.com>
2022-12-16 16:12:41 -0800

    refactor: Avoid duplicating Supabase URL environment variable

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,7 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
-import { SUPABASE_URL, SUPABASE_SERVICE_KEY } from "$env/static/private";
+import { SUPABASE_SERVICE_KEY } from "$env/static/private";
+import { PUBLIC_SUPABASE_URL } from "$env/static/public";
 
 export default createClient(
-  SUPABASE_URL,
+  PUBLIC_SUPABASE_URL,
   SUPABASE_SERVICE_KEY,
 );

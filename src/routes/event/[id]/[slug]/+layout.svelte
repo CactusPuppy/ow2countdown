288b5a6de6f629ddf0d455d1b33c0a34e1a46389
CactusPuppy <puppycactus@gmail.com>
2022-12-26 12:00:41 -0800

    feat: Add transition on background colors

--- src/routes/event/[id]/[slug]/+layout.svelte
+++ src/routes/event/[id]/[slug]/+layout.svelte
@@ -8,7 +8,7 @@
 >
   <slot />
   <a href="/"
-  class="px-6 py-3 md:px-8 md:py-4 text-lg md:text-xl focus:underline hover:underline text-ow2-orange dark:text-ow2-light-orange max-w-3xl rounded-md bg-zinc-200 hover:bg-zinc-300 dark:bg-zinc-800 dark:hover:bg-zinc-700 cursor-pointer"
+  class="px-6 py-3 md:px-8 md:py-4 text-lg md:text-xl focus:underline hover:underline text-ow2-orange dark:text-ow2-light-orange max-w-3xl rounded-md bg-zinc-200 hover:bg-zinc-300 dark:bg-zinc-800 dark:hover:bg-zinc-700 cursor-pointer transition-colors"
   in:fade={{ duration: 500, delay: 900, easing: quintInOut }}
   >
     View All Events

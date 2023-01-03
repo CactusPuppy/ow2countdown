da1f3f385ba9917552cca62084445e20b7b7600b
CactusPuppy <puppycactus@gmail.com>
2023-01-03 10:11:06 -0800

    feat: Stop linking to inactive Twitter

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -25,7 +25,7 @@
   <footer
     class="flex justify-between gap-2 px-2 pt-4 mt-8 mb-2 w-full overflow-x-hidden overflow-y-visible border-t-[1px] border-t-zinc-400 dark:border-t-zinc-700"
   >
-    <p class="text-xs text-zinc-500 dark:text-zinc-400 italic text-left">Created by <a class="text-ow2-orange dark:text-ow2-light-orange underline" href="https://twitter.com/Cactus_Puppy" rel="noreferrer noopener" target="_blank">@Cactus_Puppy</a><br /><a href="https://github.com/CactusPuppy/ow2countdown" rel="noreferrer noopener" target="_blank" class="text-ow2-orange dark:text-ow2-light-orange underline">GitHub</a></p>
+    <p class="text-xs text-zinc-500 dark:text-zinc-400 italic text-left">Created by <span class="text-ow2-orange dark:text-ow2-light-orange">CactusPuppy</span><br /><a href="https://github.com/CactusPuppy/ow2countdown" rel="noreferrer noopener" target="_blank" class="text-ow2-orange dark:text-ow2-light-orange underline">GitHub</a></p>
     <p class="text-xs text-zinc-500 dark:text-zinc-400 italic text-right">
       This site and its creator are not affiliated with Overwatch or Blizzard Entertainment.
       <br />Overwatch 2 and the Overwatch 2 logo are ©2022 Blizzard Entertainment, Inc.

cb0e96a8184c1405c7390487d8340844e2a8c5c0
CactusPuppy <puppycactus@gmail.com>
2023-01-06 21:28:03 -0800

    chore: Add isEventHappeningNow helper function

--- src/lib/utils/event_helpers.ts
+++ src/lib/utils/event_helpers.ts
@@ -1,6 +1,17 @@
+import type { CountdownDate } from "$lib/types";
+import { parseISO } from "date-fns";
+
 export function titleToSlug(title: string) {
   return title.toLowerCase()
     .replace(/\[.*?\]/g, "")       // REMOVE STATUS MESSAGES
     .replace(/[^a-z0-9 ]/g, "")
     .replace(/ +/g, "-");
+}
+
+export function isEventHappeningNow(event: CountdownDate, now?: Date) {
+  if (now == undefined) now = new Date();
+
+  return event && event.date && event.end_date
+    && parseISO(event.date) < now
+    && parseISO(event.end_date) > now;
 }

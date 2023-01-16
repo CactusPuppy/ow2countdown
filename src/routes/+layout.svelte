2003593ac133ba596b723e75d01d0e377bc3a59d
CactusPuppy <puppycactus@gmail.com>
2023-01-16 06:18:46 +0000

    feat: Add Privacy Policy

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -38,8 +38,11 @@
   <footer
     class="flex justify-between gap-2 px-2 pt-4 mt-8 mb-2 w-full overflow-y-visible border-t-[1px] border-t-zinc-400 dark:border-t-zinc-700"
   >
-    <p class="text-xs text-zinc-500 dark:text-zinc-400 italic text-left">Created by CactusPuppy<br /><a href="https://github.com/CactusPuppy/ow2countdown" rel="noreferrer noopener" target="_blank" class="text-ow2-orange dark:text-ow2-light-orange underline">GitHub</a></p>
-    <p class="text-xs text-zinc-500 dark:text-zinc-400 italic text-right">
+    <p class="text-xs text-zinc-500 dark:text-zinc-400 text-left">
+      Created by CactusPuppy  |  <a href="https://github.com/CactusPuppy/ow2countdown" rel="noreferrer noopener" target="_blank" class="text-ow2-orange dark:text-ow2-light-orange underline">GitHub</a><br>
+      <a href="/privacy-policy" class="text-ow2-orange dark:text-ow2-light-orange underline">Privacy Policy</a>
+    </p>
+    <p class="text-xs text-zinc-500 dark:text-zinc-400 text-right">
       This site and its creator are not affiliated with Overwatch or Blizzard Entertainment.
       <br />Overwatch 2 and the Overwatch 2 logo are ©2022 Blizzard Entertainment, Inc.
     </p>
---
+++ src/routes/privacy-policy/+page.svelte
@@ -0,0 +1,14 @@
+<div class="mt-16 dark:text-zinc-50 text-xl">
+  <h1 class="text-3xl font-bold text-ow2-orange dark:text-ow2-light-orange mb-4">OW2Countdown's Privacy Policy</h1>
+  <p class="my-2">OW2Countdown.com does not knowingly collect any information which can personally identify you. The only information this site collects is via the Vercel Analytics API, which collects the following information:</p>
+  <ul class="ml-4 list-inside list-disc">
+    <li>The page you visited</li>
+    <li>Your general network speed (e.g. 4G, 2G)</li>
+    <li>The type of browser you are visiting from (e.g. Chrome 86, Firefox 107)</li>
+    <li>Device type (e.g. Mobile, Desktop)</li>
+    <li>Device OS (e.g. Android 10, Windows 11)</li>
+    <li>Country (e.g. US, UK)</li>
+    <li>Time of event receipt (e.g. 2020-10-31 09:06:30)</li>
+  </ul>
+  <p class="my-2">If you'd like to learn more about how Vercel processes this data on behalf of OW2Countdown.com, please read <a target="_blank" rel="noreferrer noopener" class="underline text-ow2-orange dark:text-ow2-light-orange" href="https://vercel.com/legal/privacy-policy">Vercel's Privacy Policy</a>.</p>
+</div>

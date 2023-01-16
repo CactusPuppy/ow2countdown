fa12a1632741a5a58f0b177f52ff56b8d221181c
CactusPuppy <puppycactus@gmail.com>
2023-01-16 11:53:36 -0800

    ci: Properly inline Vercel Analytics ID

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ const config = {
         plugins: [sveltekit()],
         ssr: {
                 noExternal: ['@fortawesome/free-solid-svg-icons', '@fortawesome/free-brands-svg-icons'],
+        },
+        define: {
+                'import.meta.env.VERCEL_ANALYTICS_ID': JSON.stringify(process.env.VERCEL_ANALYTICS_ID)
         }
 };
 

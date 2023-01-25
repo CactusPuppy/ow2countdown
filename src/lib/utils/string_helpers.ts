b4f9463650a5eb6379a45fb87f24ca8063bd1e3a
CactusPuppy <puppycactus@gmail.com>
2023-01-24 18:53:11 -0800

    build: Fix finnicky imports with CJS/ESM interop

--- src/lib/utils/string_helpers.ts
+++ src/lib/utils/string_helpers.ts
@@ -1,5 +1,5 @@
 import { marked } from "marked";
-import * as sanitizeHtml from "sanitize-html";
+import sanitizeHtml from "sanitize-html";
 
 export function markdownToPlaintext(markdownString: string, markedOptions: marked.MarkedOptions = {}, sanitizeHtmlOptions: sanitizeHtml.IOptions = { allowedTags: [], allowedAttributes: {} }) {
   return sanitizeHtml(marked.parse(markdownString, markedOptions), sanitizeHtmlOptions);

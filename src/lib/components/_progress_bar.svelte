d91c47a52d0b07b3ea78e8daaee06a38aff09c9b
CactusPuppy <puppycactus@gmail.com>
2023-03-03 10:34:09 -0800

    fix: Ensure small values of progress are actually still visible
    Admittedly this does mean it is slightly inaccurate, but whatever. Sue me.

--- src/lib/components/_progress_bar.svelte
+++ src/lib/components/_progress_bar.svelte
@@ -15,5 +15,5 @@
 </script>
 
 <div class="w-full bg-zinc-300 dark:bg-zinc-700 rounded-full p-1 flex flex-row">
-  <div class="bg-gradient-to-r from-ow2-orange to-ow2-light-orange dark:from-ow2-light-orange dark:to-ow2-orange h-2 rounded-full" style="width: {$usedProgress}%"></div>
+  <div class="bg-gradient-to-r from-ow2-orange to-ow2-light-orange dark:from-ow2-light-orange dark:to-ow2-orange h-2 min-w-[0.5rem] rounded-full" style="width: {$usedProgress}%"></div>
 </div>

350965b926ae3f25daf071f612612286c0582c94
CactusPuppy <puppycactus@gmail.com>
2023-03-10 14:25:54 -0800

    fix: Prevent OG description from failing with no description

--- src/lib/utils/string_helpers.ts
+++ src/lib/utils/string_helpers.ts
@@ -2,5 +2,5 @@ import { marked } from "marked";
 import sanitizeHtml from "sanitize-html";
 
 export function markdownToPlaintext(markdownString: string, markedOptions: marked.MarkedOptions = {}, sanitizeHtmlOptions: sanitizeHtml.IOptions = { allowedTags: [], allowedAttributes: {} }) {
-  return sanitizeHtml(marked.parse(markdownString, markedOptions), sanitizeHtmlOptions);
+  return sanitizeHtml(marked.parse(markdownString ?? "", markedOptions), sanitizeHtmlOptions);
 }

2cda9e22b0c9f8c16d807ab7a4ac3adaae7d1180
CactusPuppy <puppycactus@gmail.com>
2023-08-30 16:18:37 -0700

    feat: Remove redundant Home button

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -23,11 +23,4 @@
   >
     Page Not Found
   </p>
-  <a
-    class="self-center px-4 py-2 my-4 rounded bg-zinc-200 dark:bg-zinc-800 hover:bg-zinc-300 dark:hover:bg-zinc-700 text-center text-lg md:text-xl lg:text-2xl dark:text-zinc-50 hover:underline hover:text-ow2-orange dark:hover:text-ow2-light-orange transition-colors"
-    in:fade={{duration: 500, delay: 1000, easing: quintInOut}}
-    out:fade
-    href="/">
-    Home
-  </a>
 </div>

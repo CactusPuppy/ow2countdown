06d46f8efc363b2665c7a0bb53fe32d41ab1387e
CactusPuppy <puppycactus@gmail.com>
2025-07-31 19:23:10 -0700

    build(yarn): replace deprecated functionality in marked

--- src/lib/utils/string_helpers.ts
+++ src/lib/utils/string_helpers.ts
@@ -1,5 +1,10 @@
 import { marked } from "marked";
+import { mangle } from "marked-mangle";
+import { gfmHeadingId } from "marked-gfm-heading-id";
 import sanitizeHtml from "sanitize-html";
+
+marked.use(mangle());
+marked.use(gfmHeadingId())
 
 export function markdownToPlaintext(markdownString: string, sanitizeHtmlOptions: sanitizeHtml.IOptions = { allowedTags: [], allowedAttributes: {} }) {
   return sanitizeHtml(marked.parse(markdownString ?? ""), sanitizeHtmlOptions);

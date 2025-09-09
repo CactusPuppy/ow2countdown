93fba38ca9bcf1728a9da93c524b0df5445e1ee6
CactusPuppy <puppycactus@gmail.com>
2025-09-09 13:21:02 -0700

    fix: incorrect loading of vercel insights

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,6 +1,3 @@
 import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
-import type { LayoutLoad } from './$types';
 
-export const load: LayoutLoad = () => {
-  injectSpeedInsights();
-}
+injectSpeedInsights();

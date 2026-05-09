cd7fb3dc3b1b742814f3ef69e67335e92003506c
CactusPuppy <puppycactus@gmail.com>
2026-05-09 10:28:04 -0700

    build: update to using Vercel Analytics over sunset Vercel Speed Insights

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,3 +1,4 @@
-import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+import { dev } from "$app/environment";
+import { injectAnalytics } from "@vercel/analytics/sveltekit";
 
-injectSpeedInsights();
+injectAnalytics({ mode: dev ? "development" : "production" });
